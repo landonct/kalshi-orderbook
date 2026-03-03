d052ac90a744518b35ee14b46cb417f38f5144d8
landonct <120686608+landonct@users.noreply.github.com>
2026-03-02 22:51:15 -0500

    start kalshi connection

---
+++ connect.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import requests
+
+# https://kalshi.com/markets/kxfedmention/fed-mention/kxfedmention-26mar
+markets_url = f"https://api.elections.kalshi.com/trade-api/v2/markets?series_ticker=KXFEDMENTION&status=open"  # noqa: F541
+markets_response = requests.get(markets_url)
+markets_data = markets_response.json()
+
+markets_json = pd.read_json(markets_url).markets
