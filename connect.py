326c57a6f2c71c2373667b87f416d191fa25e7a2
landonct <120686608+landonct@users.noreply.github.com>
2026-03-16 22:39:50 -0400

    Plot top 3 volumes

--- connect.py
+++ connect.py
@@ -1,12 +1,18 @@
 # TODO: Chart the series and simulate the orderbook
 import pandas as pd
 import time
+
 # import numpy as np
 import requests
 import seaborn as sns
 
 BASEURL = "https://api.elections.kalshi.com/trade-api/v2"
 TICKER = "KXFEDMENTION"
+
+
+def summary_stat(df: pd.DataFrame) -> pd.DataFrame:
+    df.agg()
+
 
 def get_market(series_ticker: str) -> list[dict]:
     """Queries the Kalshi markets API to get all the current market data
@@ -17,6 +23,7 @@ def get_market(series_ticker: str) -> list[dict]:
     response.raise_for_status()
     return response.json()["markets"]
 
+
 def get_trades(ticker: str) -> pd.DataFrame:
     url = f"{BASEURL}/markets/trades"
     all_trades = []
@@ -25,7 +32,7 @@ def get_trades(ticker: str) -> pd.DataFrame:
         params = {"ticker": ticker, "limit": 1000}
         if cursor:
             params["cursor"] = cursor
-        
+
         response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
@@ -40,14 +47,14 @@ def get_trades(ticker: str) -> pd.DataFrame:
         if not cursor:
             break
 
-        time.sleep(.1)
+        time.sleep(0.1)
 
     if not all_trades:
         return pd.DataFrame()
-        
+
     df = pd.DataFrame(all_trades)
     df["created_time"] = pd.to_datetime(df["created_time"])
-    df = df.sort_values("created_time").reset_index(drop = True)
+    df = df.sort_values("created_time").reset_index(drop=True)
     mask = df.columns.str.contains(r"dollars|fp")
     df[df.columns[mask]] = df.loc[:, mask].apply(pd.to_numeric)
     return df
@@ -75,12 +82,26 @@ full_frame["word"].sort_values().unique()
 
 full_frame_filtered = full_frame[full_frame["word"] == "CRED"].copy()
 mask = full_frame_filtered.columns.str.contains(r"dollars|fp")
-full_frame_filtered[full_frame_filtered.columns[mask]] = full_frame_filtered.loc[:, mask].apply(pd.to_numeric)
-sns.lineplot(
-    data=full_frame_filtered,
-    x="created_time", y="yes_price_dollars"
-    )
+full_frame_filtered[full_frame_filtered.columns[mask]] = full_frame_filtered.loc[
+    :, mask
+].apply(pd.to_numeric)
+sns.lineplot(data=full_frame_filtered, x="created_time", y="yes_price_dollars")
 
-df_volume = full_frame[(full_frame["yes_price_dollars"] < .8) & (full_frame["yes_price_dollars"] > .2)].groupby("word").agg(volume=("count_fp", "sum")).sort_values(by="volume", ascending=False).reset_index()
+df_volume = (
+    full_frame[
+        (full_frame["yes_price_dollars"] < 0.8)
+        & (full_frame["yes_price_dollars"] > 0.2)
+    ]
+    .groupby("word")
+    .agg(volume=("count_fp", "sum"))
+    .sort_values(by="volume", ascending=False)
+    .reset_index()
+)
 top_3_volume = df_volume["word"].head(3).to_list()
 filtered_top_3_volume = full_frame[full_frame["word"].isin(top_3_volume)]
+
+sns.lineplot(
+    data=filtered_top_3_volume, x="created_time", y="yes_price_dollars", hue="word"
+)
+
+filtered_top_3_volume.set_index("created_time").resample("5min").last().ffill()
